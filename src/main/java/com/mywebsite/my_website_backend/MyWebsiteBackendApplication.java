6627ce6b1502c78350c91303da9df0aebd73bbda
dahyunhub <tls848@naver.com>
2025-12-02 00:14:05 +0900

    first commit

---
+++ src/main/java/com/mywebsite/my_website_backend/MyWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mywebsite.my_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mywebsite/my_website_backend/MyWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywebsite.my_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
